ecde3ae206a3f8e15c2f666382dfb6d80852ae37
tooManyWires <45677378+tooManyWires@users.noreply.github.com>
2022-03-16 23:07:05 -0400

    Add files via upload

---
+++ bulkCommand.ps1
@@ -0,0 +1,13 @@
+﻿param (
+    [parameter(Mandatory=$true)][string]$file,
+    [parameter(Mandatory=$true)][string]$cmd,
+    [parameter(Mandatory=$false)][string]$domain
+)
+
+$pcs = GC $file # get the list of pcs
+Foreach ($pc in $pcs)	{
+    if($domain -and !$pc.contains($domain)){#if the domain is set and doesnt exist at the end of the pc name
+       $pc="$pc.$domain"
+    }
+    Invoke-Command -ComputerName $pc -ScriptBlock{ $cmd }
+}
